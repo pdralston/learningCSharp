97629b81a9cd78e9eef22857bd48831c841c9026
Perry David Ralston Jr <ralstontech@gmail.com>
2021-08-02 14:30:41 -0700

    Console Properties and Methods exploration

--- variables_lesson4/variables_lesson4/Program.cs
+++ variables_lesson4/variables_lesson4/Program.cs
@@ -4,9 +4,40 @@ namespace variables_lesson4
 {
     class Program
     {
+        static int count = 2;
+        /// <summary>
+        /// Console Methods
+        /// </summary>
+        /// <param name="args">
+        /// Command line args
+        /// </param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //avoid infinite loops
+            if (count == 0) return;
+            --count;
+            int answer = 42;
+            //print string arg without adding newline
+            Console.Write("text here");
+            //print string arg, add newline
+            Console.WriteLine("text here");
+            //Reads a single char and returns the ASCII Value
+            Console.Read();
+            //Read an entire line of input and return a string
+            Console.ReadLine();
+            //Captures the next keypress and returns a ConsoleKeyInfo object
+            Console.ReadKey();
+            //assigning a formatted string, note the $ to signify interpolated string
+            string formattedString = $"This works because {answer} is the answer to life the universe and everything";
+            Console.WriteLine(formattedString);
+
+            //additional Console methods/properties explored
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            
+            //see the effects of the additional console method calls
+            Main(null);
         }
     }
 }
