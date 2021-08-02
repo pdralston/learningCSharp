5b5b4fb113960f2a47975a6bfdff0c41b44aeebe
Perry David Ralston Jr <ralstontech@gmail.com>
2021-08-02 11:03:43 -0700

    string lesson completed

--- variables_lesson3/variables_lesson3/Program.cs
+++ variables_lesson3/variables_lesson3/Program.cs
@@ -4,10 +4,20 @@ namespace variables_lesson3
 {
     class Program
     {
+        ///<summary>
+        /// This is an XML doc comment
+        /// </summary>
         static void Main(string[] args)
         {
             string myname = "David";
-            Console.WriteLine("My name is " + myname);
+            string message = "My name is " + myname;
+
+            string capsMessage = message.ToUpper();
+            string lowerMessage = message.ToLower();
+
+            Console.WriteLine(message);
+            Console.WriteLine(capsMessage);
+            Console.WriteLine(lowerMessage);
         }
     }
 }
