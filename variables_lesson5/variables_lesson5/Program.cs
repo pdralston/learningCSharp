60314ec94d04ff2d18d353564fa994d00f15b9e2
Perry David Ralston Jr <ralstontech@gmail.com>
2021-08-02 14:39:47 -0700

    More data type and variable exploration

---
+++ variables_lesson5/variables_lesson5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace variables_lesson5
+{
+    //Classes and Methods use PascalCasing U____I____
+    //Classes use noun names
+    //Methods use verb names
+    class Program
+    {
+        //args/local vars are camelCased
+        //avoid abreviations: userControl vs usrCtrl
+        //avoid underscore: car_engine
+        //exception to underscore is leading underscore _loginName is ok!
+        //more consistent to use the primitives over classes
+        static void Main(string[] args)
+        {
+            //implicit and explicit conversions
+            double myDouble = 13.37;
+            int myInt;
+
+            //cast double to int
+            myInt = (int)myDouble;
+            Console.WriteLine(myInt);
+
+        }
+    }
+}
