105f76cff61bf2b10ed6d2c99e3b4d2b6988b87b
Perry David Ralston Jr <ralstontech@gmail.com>
2021-08-02 11:05:04 -0700

    Value and reference variables

---
+++ variables_lesson4/variables_lesson4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace variables_lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
