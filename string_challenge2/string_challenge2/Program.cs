e0804b75754806b2beeb37930948e8e35b889c6f
Perry David Ralston Jr <ralstontech@gmail.com>
2021-08-03 00:51:30 -0700

    finished challenge

--- string_challenge2/string_challenge2/Program.cs
+++ string_challenge2/string_challenge2/Program.cs
@@ -6,7 +6,26 @@ namespace string_challenge2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string value, firstName, lastName, fullName;
+            char searchValue;
+            int index;
+            Console.WriteLine("Enter a string here: ");
+            value = Console.ReadLine();
+            Console.WriteLine("Enter the character to search: ");
+            searchValue = (char)Console.ReadKey(true).Key;
+            index = value.IndexOf(searchValue);
+            if (searchValue >= 0)
+            {
+                Console.WriteLine($"{searchValue} found at index {index}");
+            } else {
+                Console.WriteLine("Charecter not found in String");
+            }
+            Console.WriteLine("Enter your first name : ");
+            firstName = Console.ReadLine();
+            Console.WriteLine("Enter your last name : ");
+            lastName = Console.ReadLine();
+            fullName = String.Concat(firstName, " ", lastName);
+            Console.WriteLine($"Thank you {fullName}");
         }
     }
 }
