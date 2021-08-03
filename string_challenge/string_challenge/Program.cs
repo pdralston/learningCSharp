2d1e34856064b38b0837ef67253bdb52ce2e2a23
Perry David Ralston Jr <ralstontech@gmail.com>
2021-08-02 23:51:21 -0700

    Challenge complete

---
+++ string_challenge/string_challenge/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace string_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Please enter your name and press enter");
+            name = Console.ReadLine();
+            Console.WriteLine(name.ToUpper());
+            Console.WriteLine(name.ToLower());
+            Console.WriteLine(name.Trim());
+            Console.WriteLine(name.Substring(2));
+        }
+    }
+}
