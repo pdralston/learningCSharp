b868189080e02315e98eff7cdd63b7107441920d
Perry David Ralston Jr <ralstontech@gmail.com>
2021-08-06 12:02:38 -0700

    increment and decrement operators

--- Operators/Operators/Program.cs
+++ Operators/Operators/Program.cs
@@ -4,9 +4,28 @@ namespace Operators
 {
     class Program
     {
+       static int num = 0, num1 = 5, num2 = 3, num3;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //unary operators
+            num3 = -num1; //unary - multiplies right hand side by -1
+            Console.WriteLine($"num3 is {num3}");
+            
+            bool isSunny = true;
+            Console.WriteLine($"is it sunny? {!isSunny}"); //! operator negates the right hand side
+
+            //increment operator
+            num++;
+            Console.WriteLine($"num is {num}");
+            Console.WriteLine($"num is {num++}");//num prints as 1 here
+            Console.WriteLine($"num is {++num}");//num prints as 3 here
+
+            //increment operator
+            num--;
+            Console.WriteLine($"num is {num}");
+            Console.WriteLine($"num is {num--}");//num prints as 2 here
+            Console.WriteLine($"num is {--num}");//num prints as 0 here
+
         }
     }
 }
