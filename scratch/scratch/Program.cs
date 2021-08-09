9890aa2cc59f1bf289254bad27446aaa717ffdf1
Perry David Ralston Jr <ralstontech@gmail.com>
2021-08-09 08:57:40 -0700

    some scratch work

---
+++ scratch/scratch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace scratch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Reading a charecter and printing it back to console: {Console.ReadKey(true).Key.ToString()}");
+        }
+    }
+}
