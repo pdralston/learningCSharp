04d772804f32c1a9749dc4b7465af7a3259a2968
Perry David Ralston Jr <ralstontech@gmail.com>
2021-08-11 06:10:08 -0700

    Challenge complete

---
+++ PropertiesChallenge/PropertiesChallenge/Box.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesChallenge
+{
+    class Box
+    {
+        private int height;
+        private int width;
+        private int length;
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                height = value > 0 ? value : -value;
+            }
+        }
+        public int Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                width = value > 0 ? value : -value;
+            }
+        }
+        public int Length
+        {
+            get
+            {
+                return length;
+            }
+            set
+            {
+                length = value > 0 ? value : -value;
+            }
+        }
+        public int Volume
+        {
+            get
+            {
+                return Height * Width * Length;
+            }
+        }
+        public int FaceArea
+        {
+            get
+            {
+                return Height * Width;
+            }
+        }
+
+        public Box(int height = 1, int width = 1, int length = 1)
+        {
+            Height = height;
+            Width = width;
+            Length = length;
+        }
+
+        public override string ToString()
+        {
+            return $"Box Dimensions\nHeight: {Height}\nWidth: {Width}\nLength: {Length}\nFaceArea: {FaceArea}\nVolume: {Volume}";
+        }
+    }
+}
--- PropertiesChallenge/PropertiesChallenge/Program.cs
+++ PropertiesChallenge/PropertiesChallenge/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 
 namespace PropertiesChallenge
-{
+{   
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Box box1 = new Box();
+            Box box2 = new Box(1,2,3);
+            Console.WriteLine($"{box1}\n\n{box2}");
+
         }
     }
 }
