bb715f17cbb115b863afc9049f0173e26b052f50
Perry David Ralston Jr <ralstontech@gmail.com>
2021-08-12 23:04:03 -0700

    TicTacToe Done

--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -6,7 +6,19 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Board game = new Board();
+            bool playGame = true;
+            while (playGame)
+            {
+                game.Play();
+                Console.Write("Press any key to continue, Esc to close: ");
+                playGame = !Console.ReadKey(true).Key.Equals(ConsoleKey.Escape);
+                if (playGame)
+                {
+                    game.ResetBoard();
+                }
+            }
+            
         }
     }
 }
